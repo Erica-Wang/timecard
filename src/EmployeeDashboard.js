1b2a35588bc0524f3d4193c016fb506919bb686e
Rahma Gillan <rahmagillan2001@gmail.com>
2020-04-19 19:50:26 -0400

    Auto stash before merge of "master" and "origin/master"

---
+++ src/EmployeeDashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function EmployeeDashboard() {
+  return (
+    <div>
+      <h1>Employee Dashboard</h1>
+    </div>
+  );
+}
+
+export default EmployeeDashboard;
---
+++ src/ManagerLogin.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function ManagerLogin() {
+  return (
+    <div>
+      <h1>Manager Login</h1>
+    </div>
+  );
+}
+
+export default ManagerLogin;
