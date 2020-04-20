4e553fb0b09d9ed8fa095f69f0b05f45dae073f7
Rahma Gillan <rahmagillan2001@gmail.com>
2020-04-19 20:45:43 -0400

    Links to pages

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import {Link} from 'react-router-dom';
 
 function Home() {
   return (
     <div>
       <h1>Home Page</h1>
+      <Link to='/login'>
+        <h2>Login</h2>
+      </Link>
     </div>
   );
 }
--- src/Login.js
+++ src/Login.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
+import {Link} from 'react-router-dom';
 
 function Login() {
   return (
     <div>
       <h1>General Login</h1>
       <h3>IDs will be associated with role type</h3>
+      <Link to='/employee-dashboard'> 
+      {/* Change the value of to depending on type of account*/}
+        <h2>Login</h2>
+      </Link>
     </div>
   );
 }
