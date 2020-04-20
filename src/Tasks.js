57a148fb7c683bb40728be6bc807c92ce4549221
Rahma Gillan <rahmagillan2001@gmail.com>
2020-04-20 01:01:26 -0400

    it renders finally

--- src/Tasks.js
+++ src/Tasks.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {Button} from 'react-bootstrap';
+import Timecard from './Timecard';
+
 
 const Tasks = (props) => {
+  const [tc, setTc] = useState();
   return (
     <div className="task">
       <p className="task-att">Job: {props.job}</p>
@@ -12,7 +15,8 @@ const Tasks = (props) => {
       <p className="task-att">Notes: {props.notes}</p>
       <p className="task-att">Assigning Manager: {props.manager}</p>
       <p className="task-att">Time Code: {props.timeCode}</p>
-      <Button className="task-btn">Task Complete</Button>
+      <Button className="task-btn" onClick={() => setTc((<Timecard />))}>Task Complete</Button>
+      {tc}
     </div>
   );
 }
