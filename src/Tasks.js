772345430ea4f922ee7e617407f78672d60a2e75
Tailai Wang <tailaiwang01@gmail.com>
2020-04-20 01:19:43 -0400

    Dan React Parser

--- src/Tasks.js
+++ src/Tasks.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import './App.css';
 import {Button} from 'react-bootstrap';
+import ReactHtmlParser from "react-html-parser";
 
+const loops = (jsonData) =>{
+  var output = "";
+  for (var i = 0; i < jsonData.length(); i++){
+    output += "<div className='task'>" +
+    "<p className='task-att'>Job: "+jsonData.job+"</p>" +
+    "<p className='task-att'>Job Code: "+props.jobCode+ "</p>" +
+    "<p className='task-att'>Activity: "+props.activity+ "</p>" +
+    "<p className='task-att'>Activity Code: "+props.activityCode +"</p>" +
+    "<p className='task-att'>Notes: "+props.notes+"</p>" +
+    "<p className='task-att'>Assigning Manager: {props.manager}</p>" +
+    "<p className='task-att'>Time Code: :"+ props.timeCode+"</p>" +
+    "<Button className='task-btn'>Task Complete</Button>" +
+  "</div>"
+  }
+}
 const Tasks = (props) => {
   return (
-    <div className="task">
-      <p className="task-att">Job: {props.job}</p>
-      <p className="task-att">Job Code: {props.jobCode} </p>
-      <p className="task-att">Activity: {props.activity} </p>
-      <p className="task-att">Activity Code: {props.activityCode}</p>
-      <p className="task-att">Notes: {props.notes}</p>
-      <p className="task-att">Assigning Manager: {props.manager}</p>
-      <p className="task-att">Time Code: {props.timeCode}</p>
-      <Button className="task-btn">Task Complete</Button>
-    </div>
+    ReactHtmlParser(loops(jsonData))
   );
 }
 
