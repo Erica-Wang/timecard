93ab1cefd314988c2d5a2e9b808781f8bf9dc0af
Rahma Gillan <rahmagillan2001@gmail.com>
2020-04-19 21:58:18 -0400

    New pages in employee dashboard

--- src/EmployeeDashboard.js
+++ src/EmployeeDashboard.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
+import {Link} from 'react-router-dom';
 
 function EmployeeDashboard() {
   return (
     <div>
       <h1>Employee Dashboard</h1>
+      <Link to="tasks">
+        <h2>Tasks</h2>
+      </Link>
+      <Link to="timecard">
+        <h2>Timecard</h2>
+      </Link>
     </div>
   );
 }
---
+++ src/Tasks.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function Tasks() {
+  return (
+    <div>
+      <h1>Tasks</h1>
+    </div>
+  );
+}
+
+export default Tasks;
