fcc1d8eebee7be6cc773be3273e87e21b312d9f1
Tailai Wang <tailaiwang01@gmail.com>
2020-04-19 20:15:34 -0400

    Moving to 1 Page Login

---
+++ src/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function Login() {
+  return (
+    <div>
+      <h1>General Login</h1>
+      <h3>IDs will be associated with role type</h3>
+    </div>
+  );
+}
+
+export default Login;
