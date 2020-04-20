818c3fe7381ef14f7737f9fe9fcf595435406df9
Rahma Gillan <rahmagillan2001@gmail.com>
2020-04-19 22:56:20 -0400

    task component

--- src/Tasks.js
+++ src/Tasks.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
+import {Button} from 'react-bootstrap';
 
 function Tasks() {
   return (
-    <div>
-      <h1>Tasks</h1>
+    <div className="task">
+      <p className="task-att">Job: </p>
+      <p className="task-att">Job Code: </p>
+      <p className="task-att">Activity: </p>
+      <p className="task-att">Job Code: </p>
+      <p className="task-att">Notes: </p>
+      <p className="task-att">Assigning Manager: </p>
+      <p className="task-att">Time Code: </p>
+      <Button className="task-btn">Task Complete</Button>
     </div>
   );
 }
