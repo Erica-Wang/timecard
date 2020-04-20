77dcd5f1ed02add211a6b705cdeef212191e90cd
Rahma Gillan <rahmagillan2001@gmail.com>
2020-04-19 21:20:22 -0400

    Home Page CSS
    my shards is not working :(

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import {Link} from 'react-router-dom';
+import logo from './assets/logo.svg';
 
 function Home() {
   return (
     <div>
-      <h1>Home Page</h1>
+      <img src={logo} className="logo" alt="Logo" />
+      <h1 className="subtitle">Operations Timecard Manager</h1>
       <Link to='/login'>
         <h2>Login</h2>
       </Link>
