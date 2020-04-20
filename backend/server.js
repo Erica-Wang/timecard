0ebbda288198b4885ee70850971550071085325f
Erica Wang <ericawang@Ericas-MacBook-Pro.local>
2020-04-19 20:05:38 -0400

    backend node project setup

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cors());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+
+const server = app.listen(5000, function(){
+ console.log('Listening on port 5000');
+});
