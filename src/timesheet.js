72a34f64da24e295c85589571b2a635da5891615
Daniel Lu <daniel020301@gmail.com>
2020-04-21 01:47:37 -0400

    semi-working

--- src/timesheet.js
+++ src/timesheet.js
@@ -3,19 +3,19 @@ import './App.css';
 import {Button} from 'react-bootstrap';
 
 
+/*
+//props:
+//name
+//
 const timeSheet = (props) => {
   const [tc, setTc] = useState();
 
   return (
     <div className="timeSheet">
-      <p className="task-att">Job: {props.jobCode}</p>
-      <p className="task-att">Activity: {props.activityCode} </p>
-      <p className="task-att">Notes: {props.notes}</p>
-      <p className="task-att">Assigning Manager: {props.managerAssigned}</p>
-      <Button className="task-btn" onClick={() => setTc((<Timecard />))}>Task Complete</Button>
-      {tc}
+      <h1>Name: {props.id} </h1>
+      {taskList}
     </div>
   );
 }
 
-export default Tasks;
+export default Tasks;*/
