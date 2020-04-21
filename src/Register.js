1438c18cb3838b92907371575a51bf0ad82c0b41
Hong Yi Chen <hongyigma@gmail.com>
2020-04-21 03:18:51 -0400

    passing user id to dashboards

--- src/Register.js
+++ src/Register.js
@@ -1,28 +1,78 @@
 import React, { useState } from "react";
-import { Button, FormGroup, FormControl, FormCheck, ToggleButton, ToggleButtonGroup } from "react-bootstrap";
-import { Link } from 'react-router-dom';
+import { Button, FormGroup, FormControl, FormCheck, DropdownButton, Dropdown } from "react-bootstrap";
+import { Redirect } from 'react-router-dom';
+import axios from 'axios';
 import './App.css';
 
 function Register() {
   const [userid, setUserid] = useState("");
   const [password, setPassword] = useState("");
+  const [redir, setRedir] = useState();
+  var manager = false;
+  var basePay = 0;
 
   function validateForm() {
     return userid.length === 6 && password.length > 0;
   }
 
-  function handleSubmit(event) {
-    event.preventDefault();
+  function checkManager() {
+    if (manager) {
+      manager = false;
+    } else {
+      manager = true;
+    }
+  }
+
+  function handleSubmit() {
+    console.log("hello");
+
+    if (manager) {
+      axios.get('https://htc2020-timecard.herokuapp.com/managerlogin', {
+        params: {
+          id: userid,
+          password: password,
+        }
+      })
+        .then(res => {
+          if (res.data.auth === 'true') {
+            console.log(res);
+            setRedir(<Redirect to={{ pathname: '/manager-dashboard', data: { userid: userid } }} />);
+          } else {
+            alert("Invalid user ID or password, please try again!");
+          }
+        }).catch((err) => {
+          console.log(err);
+        });
+    } else {
+      axios.get('https://htc2020-timecard.herokuapp.com/workerlogin', {
+        params: {
+          id: userid,
+          password: password,
+        }
+      })
+        .then(res => {
+          if (res.data.auth === 'true') {
+            console.log(res);
+            setRedir(<Redirect to={{ pathname: '/employee-dashboard', data: { userid: userid } }} />);
+          } else {
+            alert("Invalid user ID or password, please try again!");
+          }
+        }).catch((err) => {
+          console.log(err);
+        });
+
+    }
   }
 
   return (
     <div className="Login">
-      <form onSubmit={handleSubmit}>
-        <h2>General Login</h2>
-        <h4>IDs will be associated with role type</h4>
+      {redir}
+      <form >
+        <h2>New User Registration</h2>
+        <h4>Enter in your registration information</h4>
 
         <FormGroup controlId="userid" bsSize="large">
-          User ID
+          Full Name
         <FormControl
             autoFocus
             type="userid"
@@ -40,13 +90,28 @@ function Register() {
           />
         </FormGroup>
 
-        <Link to='/employee-dashboard'>
-          <Button block bsSize="large" disabled={!validateForm()} type="submit">create account</Button>
-          {
-              // you want to render in their userID
-              // you want to create a button that says go back to Login
-          }
-        </Link>
+        <FormCheck
+          type="switch"
+          id="custom-switch"
+          label="Check this if you're a manager"
+          className="form-check"
+          onClick={checkManager}
+        />
+
+        <DropdownButton id="dropdown-basic-button" title="What is your base pay?" className="dropdown-pay">
+            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+            <Dropdown.Item>Another action</Dropdown.Item>
+            <Dropdown.Item>Something else</Dropdown.Item>
+        </DropdownButton>
+        
+        <Button
+          block
+          bsSize="large"
+          disabled={!validateForm()}
+          onClick={() => handleSubmit()}
+        >Register
+        </Button>
+
       </form>
     </div>
 
