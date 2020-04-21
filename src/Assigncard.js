e8b117c939de10adc95c1beebd7a6286a37cdef3
Tailai Wang <tailaiwang01@gmail.com>
2020-04-21 00:09:38 -0400

    assigning with dummy JSON + toggle

---
+++ src/Assigncard.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './App.css';
+import {Form, Button} from 'react-bootstrap';
+
+const userListTest = require('./assets/testUserList.json')
+const userList = []
+for (const user of userListTest){
+  userList.push(<option >{user.name}</option>);
+}
+
+
+function Assigncard() {
+  return (
+    <div className="timecard">
+      <Form className="timecard-form">
+        <Form.Group controlId="equimentNumber">
+          <Form.Label>Worker Assigned</Form.Label>
+          <Form.Control as="select">
+                {userList}
+            </Form.Control>
+        </Form.Group>
+        <Form.Group controlId="hour">
+          <Form.Label>Notes</Form.Label>
+          <Form.Control type="" placeholder="Job Details" />
+          <Form.Text className="text-muted"></Form.Text>
+        </Form.Group>
+      <Button className="submit-btn">Submit</Button>
+      </Form>
+    </div>
+  );
+}
+
+export default Assigncard;
