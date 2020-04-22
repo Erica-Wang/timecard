7af4278e5ce85c7cb72779cf0624a0e7feac00e8
Rahma Gillan <rahmagillan2001@gmail.com>
2020-04-21 21:48:12 -0400

    some dashboard css

--- src/ManagerDashboard.js
+++ src/ManagerDashboard.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import { Link, Route } from 'react-router-dom';
 import { Container, Row, Col, Navbar } from 'react-bootstrap';
-import logo from './logo.svg'
+import logo from './assets/logo.svg';
 import Button from 'react-bootstrap/Button';
 import axios from 'axios';
 import CsvDownloader from 'react-csv-downloader';
@@ -58,50 +58,53 @@ const ManagerDashboard = (props) => {
   }];
 
   return (
-    <div class="dashboard">
-       <Navbar bg="light">
-        <Navbar.Brand>
-          <img className="nav-logo" src={logo} alt="logo" />
-        </Navbar.Brand>
-        <Navbar.Collapse className="justify-content-end">
-          <Navbar.Text>
-            Signed in as: {props.location.state.userid}
-            <Link to='/'>
-              <p style={{textAlign:"right", padding:"none"}}>Log Out</p>  
-            </Link>  
-          </Navbar.Text>
-        </Navbar.Collapse>
-      </Navbar>
-      <h1>Manager Dashboard</h1>
-      <hr></hr>
-      <h4>Manager Options</h4>
-      <Container>
-        <Row>
-          <Col className="btn-divide">
-            <Link className="btn-link" to="/manager-dashboard/assign-tasks">
-              <Button className="gen-btn" variant="success" type="submit">
-                Assign Tasks
-              </Button>
-            </Link>
-          </Col>
-          <Col className="btn-divide">
-            <Link className="btn-link" to="/manager-dashboard/validate-timesheets">
-              <Button className="gen-btn" variant="success" type="submit">
-                Validate Timesheets
-              </Button>
-            </Link>
-          </Col>
-        </Row>
-      </Container>
-      <CsvDownloader
-        filename="Worker Timesheets"
-        separator=";"
-        columns={columns}
-        datas={() => exportDatas()}
-        text="Export as CSV" />
-                      <Button className="gen-btn" variant="success" type="submit" onClick={console.log(exportDatas())}>
-                Export Datas
-              </Button>
+    <div>
+      <div className="background"></div>
+      <div className="nav-md">
+        <Navbar bg="light">
+            <Navbar.Brand>
+              <img className="nav-logo" src={logo} alt="logo" />
+            </Navbar.Brand>
+            <Navbar.Collapse className="justify-content-end">
+              <Navbar.Text>
+                Signed in as: {props.location.state.userid}
+                <Link to='/'>
+                  <p style={{textAlign:"right", padding:"none"}}>Log Out</p>  
+                </Link>  
+              </Navbar.Text>
+            </Navbar.Collapse>
+          </Navbar>
+        </div>
+     <div className="content">
+        <h4 className="subtitle">Manager Dashboard</h4>
+        <Container>
+          <Row>
+            <Col className="btn-divide">
+              <Link className="btn-link" to="/manager-dashboard/assign-tasks">
+                <Button className="gen-btn" variant="success" type="submit">
+                  Assign Tasks
+                </Button>
+              </Link>
+            </Col>
+            <Col className="btn-divide">
+              <Link className="btn-link" to="/manager-dashboard/validate-timesheets">
+                <Button className="gen-btn" variant="success" type="submit">
+                  Validate Timesheets
+                </Button>
+              </Link>
+            </Col>
+          </Row>
+        </Container>
+        <CsvDownloader
+          filename="Worker Timesheets"
+          separator=";"
+          columns={columns}
+          datas={() => exportDatas()}
+          text="Export as CSV" />
+                        <Button className="gen-btn" variant="success" type="submit" onClick={console.log(exportDatas())}>
+                  Export Datas
+                </Button>
+      </div>
     </div>
     
   );
