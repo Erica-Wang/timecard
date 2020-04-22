4ed83580d961c580c1d21e25159fb6a1feb4d258
Hong Yi Chen <hongyigma@gmail.com>
2020-04-21 22:31:08 -0400

    merge fix

--- src/ManagerDashboard.js
+++ src/ManagerDashboard.js
@@ -13,25 +13,25 @@ const ManagerDashboard = (props) => {
 
   const exportDatas = () => {
     axios.get('https://htc2020-timecard.herokuapp.com/getcsv')
-    .then(res => {
-      const datas = [];
-      var i = 0;
-      for (i = 0; i < res.data.length; i++) {
-        const row = {
-          EmployeeName: res.data[i].EmployeeName,
-          EmployeeID: res.data[i].EmployeeID,
-          EmployeeType: res.data[i].EmployeeType,
-          Date: res.data[i].Date,
-          JobCode: res.data[i].JobCode,
-          ActivityCode: res.data[i].ActivityCode,
-          Hours: res.data[i].Hours,
-          Timecode: res.data[i].Timecode,
+      .then(res => {
+        const datas = [];
+        var i = 0;
+        for (i = 0; i < res.data.length; i++) {
+          const row = {
+            EmployeeName: res.data[i].EmployeeName,
+            EmployeeID: res.data[i].EmployeeID,
+            EmployeeType: res.data[i].EmployeeType,
+            Date: res.data[i].Date,
+            JobCode: res.data[i].JobCode,
+            ActivityCode: res.data[i].ActivityCode,
+            Hours: res.data[i].Hours,
+            Timecode: res.data[i].Timecode,
+          }
+          datas.push(row);
         }
-        datas.push(row);
-      }
-      console.log(datas);
-      setCSV(datas);
-    });
+        console.log(datas);
+        setCSV(datas);
+      });
   }
 
   useEffect(() => {
@@ -64,7 +64,7 @@ const ManagerDashboard = (props) => {
     displayName: 'Timecode'
   }];
 
-  const datas2= [{
+  const datas2 = [{
     EmployeeName: 'Rahma',
     EmployeeID: 'loves',
     EmployeeType: 'smoking',
@@ -89,20 +89,20 @@ const ManagerDashboard = (props) => {
       <div className="background"></div>
       <div className="nav-md">
         <Navbar bg="light">
-            <Navbar.Brand>
-              <img className="nav-logo" src={logo} alt="logo" />
-            </Navbar.Brand>
-            <Navbar.Collapse className="justify-content-end">
-              <Navbar.Text>
-                Signed in as: {props.location.state.userid}
-                <Link to='/'>
-                  <p style={{textAlign:"right", padding:"none"}}>Log Out</p>  
-                </Link>  
-              </Navbar.Text>
-            </Navbar.Collapse>
-          </Navbar>
-        </div>
-     <div className="content">
+          <Navbar.Brand>
+            <img className="nav-logo" src={logo} alt="logo" />
+          </Navbar.Brand>
+          <Navbar.Collapse className="justify-content-end">
+            <Navbar.Text>
+              Signed in as: {props.location.state.userid}
+              <Link to='/'>
+                <p style={{ textAlign: "right", padding: "none" }}>Log Out</p>
+              </Link>
+            </Navbar.Text>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
+      <div className="content">
         <h4 className="subtitle">Manager Dashboard</h4>
         <Container>
           <Row>
@@ -128,8 +128,8 @@ const ManagerDashboard = (props) => {
           columns={columns}
           datas={csv}
           text="Export as CSV" />
-                        <Button className="gen-btn" variant="success" type="submit" >
-                  Export Datas
+        <Button className="gen-btn" variant="success" type="submit" >
+          Export Datas
                 </Button>
       </div>
     </div>
